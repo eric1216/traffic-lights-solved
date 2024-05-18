c00bef3af1bba83fef7df4f10348d2d724220969
Eric H <ericolebusiness@gmail.com>
2024-05-17 22:38:22 -0400

    created state for light

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,24 @@
+import { useState } from 'react';
+
 export const FunctionalTrafficLight = () => {
+  const [lightState, setLightState] = useState(0);
+
+  const changeLight = () => {
+    lightState === 2 ? setLightState(0) : setLightState(lightState + 1);
+  };
+
   return (
-    <div className="traffic-light-box">
+    <div className='traffic-light-box'>
       <h2>Functional Traffic Light</h2>
-      <div className="traffic-light">
+      <div className='traffic-light'>
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${lightState === 0 ? 'red' : 'black'}`}></div>
+        <div className={`circle ${lightState === 2 ? 'yellow' : 'black'}`}></div>
+        <div className={`circle ${lightState === 1 ? 'green' : 'black'}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className='next-state-button' onClick={changeLight}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
